e85a2957f610f7bba25be9d18332c006f8919ed8
sxlc <sxlc@users.noreply.github.com>
2017-07-13 11:22:05 -0700

    Create Lc00374GuessNumberHigherOrLowerBinarySearchLogn.java

---
+++ Lc00374GuessNumberHigherOrLowerBinarySearchLogn.java
@@ -0,0 +1,23 @@
+public class Lc00374GuessNumberHigherOrLowerBinarySearchLogn {
+
+    public static void main(String[] args) {
+    
+    }
+    
+    public static int guessNumber(int n) {
+        int low = 1;
+        int high = n;
+        while (low <= high) {
+            int middle = low + (high - low) / 2;
+            int result = guess(middle);
+            if (result == -1) {
+                high = middle - 1;
+            } else if (result == 1) {
+                low = middle + 1;
+            } else {
+                return middle;
+            }
+        }
+        return -1;
+    }
+}
