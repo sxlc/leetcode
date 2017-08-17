3d091646d06480d060fda9412922fa2ab198cc8a
sxlc <sxlc@users.noreply.github.com>
2017-08-17 00:16:50 -0700

    Create Lc00044WildcardMatchingDynamicProgrammingOmn.java

---
+++ Lc00044WildcardMatchingDynamicProgrammingOmn.java
@@ -0,0 +1,41 @@
+public class Lc00044WildcardMatchingDynamicProgrammingOmn {
+    public static void main(String[] args) {
+        System.out.println(isMatch("aa", "a"));
+        System.out.println(isMatch("aa", "aa"));
+        System.out.println(isMatch("aaa", "aa"));
+        System.out.println(isMatch("aa", "*"));
+        System.out.println(isMatch("aa", "a*"));
+        System.out.println(isMatch("ab", "?*"));
+        System.out.println(isMatch("aab", "c*a*b"));
+    }
+  
+    public static boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = false;
+        }
+        
+        for (int j = 1; j <= n; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = true;
+            } else {
+                break;
+            }
+        }
+        
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p.charAt(j - 1) != '*') {
+                    dp[i][j] = dp[i - 1][j - 1] && (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?');
+                } else {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
+}
