1c0eb49532d685b5716c9bf80c5257da7750f870
sxlc <sxlc@users.noreply.github.com>
2017-08-23 22:27:43 -0700

    Create Lc00286WallsAndGatesOn4.java

---
+++ Lc00286WallsAndGatesOn4.java
@@ -0,0 +1,60 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Lc00286WallsAndGatesOn4 {
+    private static int dx[] = {0, 0, -1, 1};
+    private static int dy[] = {1, -1, 0, 0};
+  
+    public static void main(String[] args) {
+        int[][] grid = new int[][] {
+                {Integer.MAX_VALUE, -1, 0, Integer.MAX_VALUE},
+                {Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, -1},
+                {Integer.MAX_VALUE, -1, Integer.MAX_VALUE, -1},
+                {0, -1, Integer.MAX_VALUE, Integer.MAX_VALUE},
+        };
+        wallsAndGates(grid);
+        for (int[] row : grid) {
+            for (int position : row) {
+                System.out.print(String.format("%d ", position));
+            }
+            System.out.println();
+        }
+    }
+
+    public static void wallsAndGates(int[][] rooms) {
+        if (rooms == null || rooms.length == 0) {
+            return;
+        }
+        
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < rooms.length; i++) {
+            for (int j = 0; j < rooms[0].length; j++) {
+                if (rooms[i][j] == 0) {
+                    queue.add(new int[]{i, j});
+                }
+            }
+        }
+        while (!queue.isEmpty()) {
+            int[] gate = queue.poll();
+            int row = gate[0];
+            int column = gate[1];
+            if (row > 0 && rooms[row - 1][column] == Integer.MAX_VALUE) {
+                rooms[row - 1][column] = rooms[row][column] + 1;
+                queue.add(new int[]{row - 1, column});
+            }
+            if (row < rooms.length - 1 && rooms[row + 1][column] == Integer.MAX_VALUE) {
+                rooms[row + 1][column] = rooms[row][column] + 1;
+                queue.add(new int[]{row + 1, column});
+            }
+            if (column > 0 && rooms[row][column - 1] == Integer.MAX_VALUE) {
+                rooms[row][column - 1] = rooms[row][column] + 1;
+                queue.add(new int[]{row, column - 1});
+            }
+            if (column < rooms[0].length - 1 && rooms[row][column + 1] == Integer.MAX_VALUE) {
+                rooms[row][column + 1] = rooms[row][column] + 1;
+                queue.add(new int[]{row, column + 1});
+            }
+        }
+    }
+}
