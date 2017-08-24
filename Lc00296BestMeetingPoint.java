11164067bb5588163743699d92937cd5a2677051
sxlc <sxlc@users.noreply.github.com>
2017-08-24 00:01:25 -0700

    Update Lc00296BestMeetingPoint.java

--- Lc00296BestMeetingPoint.java
+++ Lc00296BestMeetingPoint.java
@@ -3,8 +3,6 @@ import java.util.Collections;
 import java.util.List;
 
 public class Lc00296BestMeetingPoint {
-    private static int dx[] = {0, 0, -1, 1};
-    private static int dy[] = {1, -1, 0, 0};
   
     public static void main(String[] args) {
         int[][] grid = new int[][] {
